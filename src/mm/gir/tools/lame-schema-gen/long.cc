846ae16aac39bfa8075d5863d57427a91ea9d06b
Krzesimir Nowak <qdlacz@gmail.com>
2015-12-11 21:45:08 +0100

    asd

--- src/mm/gir/tools/lame-schema-gen/long.cc
+++ src/mm/gir/tools/lame-schema-gen/long.cc
@@ -1,5 +1,7 @@
 #include "long.hh"
 
+#include <stdexcept>
+
 namespace Mm
 {
 
@@ -12,11 +14,95 @@ namespace Tools
 namespace LameSchemaGen
 {
 
-void
-Long::process_node_vfunc (std::string const&, pugi::xml_node const&, int)
-{}
+namespace
+{
 
-NamedSet<Long::Node>&&
+void
+process_single_element (Long::Node& child, pugi::xml_node const& node)
+{
+  // TODO
+}
+
+
+} // anonymous namespace
+
+void
+Long::process_toplevel (std::string const& name, pugi::xml_node const& node, int depth)
+{
+  if (depth > 0)
+  {
+    throw std::logic_error ("no toplevel node, but the depth is greater than zero");
+  }
+  if (!node_stack.empty ())
+  {
+    throw std::logic_error ("no toplevel node, but node stack is not empty");
+  }
+  toplevel = std::make_unique (name);
+  toplevel->min_occurences = 1;
+  toplevel->max_occurences = 1;
+}
+
+void
+Long::process_element (std::string const& name, pugi::xml_node const& node, int depth)
+{
+  if (depth == 0)
+  {
+    throw std::logic_error ("there is a toplevel node, but the depth is zero");
+  }
+  if (node_stack.empty ())
+  {
+    throw std::logic_error ("there is a toplevel node, but node stack is empty");
+  }
+  setup_node_stack_for_depth (depth);
+  auto pair = node_stack.top ().children.emplace (name, name);
+  auto& child = pair.first->second;
+  if (pair.second)
+  {
+    child.depth = depth;
+  }
+  process_single_element (child, node);
+  node_stack.push (child);
+}
+
+void
+Long::setup_node_stack_for_depth (int depth)
+{
+  auto depth_diff = depth - current_node->depth;
+
+  if (depth_diff > 0)
+  {
+    if (depth_diff > 1)
+    {
+      throw std::runtime_error ("sudden large jump in depth, expected a depth greater by one");
+    }
+  }
+  else
+  {
+    while (node_stack.top ().depth >= depth)
+    {
+      node_stack.pop ();
+      if (node_stack.empty ())
+      {
+        throw std::runtime_error ("something went wrong when getting parent for depth");
+      }
+    }
+  }
+}
+
+void
+Long::process_node_vfunc (std::string const& name, pugi::xml_node const& node, int depth)
+{
+  if (!toplevel)
+  {
+    process_toplevel (name, node, depth);
+  }
+  else
+  {
+    process_element (name, node, depth);
+  }
+}
+
+std::unique_ptr<Long::Node>&&
 Long::steal ()
 {
   return std::move (nodes);
--- src/mm/gir/tools/lame-schema-gen/long.hh
+++ src/mm/gir/tools/lame-schema-gen/long.hh
@@ -5,6 +5,8 @@
 #include "types.hh"
 
 #include <limits>
+#include <memory>
+#include <stack>
 
 namespace Mm
 {
@@ -26,19 +28,6 @@ public:
     std::size_t count = 0;
   };
 
-  enum class Leaf
-  {
-    UNDETERMINED,
-    NEVER_A_LEAF,
-    SOMETIMES_A_LEAF,
-    ALWAYS_A_LEAF
-  };
-
-  struct Leafed
-  {
-    Leaf leaf = Leaf::UNDETERMINED;
-  };
-
   struct Attribute : public Named, public Counted
   {
     enum class Type
@@ -53,31 +42,36 @@ public:
     Type type = Type::UNDETERMINED;
   };
 
-  struct Child : public Named, public Counted, public Leafed
+  struct Node : public Named, public Counted
   {
     using Named::Named;
 
+    StrMap<Attribute> attributes;
+    StrMap<Node> children;
     std::size_t min_occurences = std::numeric_limits<std::size_t>::max ();
     std::size_t max_occurences = 0;
-  };
-
-  struct Node : public Named, public Counted, public Leafed
-  {
-    using Named::Named;
-
-    NamedSet<Attribute> attributes;
-    NamedSet<Child> children;
     bool has_text = false;
+    int depth = 0;
   };
 
-  NamedSet<Node>&&
+  std::unique_ptr<Node>&&
   steal ();
 
 private:
   void
   virtual process_node_vfunc(std::string const& name, pugi::xml_node const& node, int depth) override;
 
-  NamedSet<Node> nodes;
+  void
+  process_toplevel (std::string const& name, pugi::xml_node const& node, int depth);
+
+  void
+  process_element (std::string const& name, pugi::xml_node const& node, int depth);
+
+  void
+  setup_node_stack_for_depth (int depth)
+
+  std::unique_ptr<Node> toplevel;
+  std::stack<Node&> node_stack;
 };
 
 } // namespace LameSchemaGen
